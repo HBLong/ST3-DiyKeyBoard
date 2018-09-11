58c10b8a2fd5fd56a6b1d32b8ebed610b23395d7
HBLong <172537247@qq.com>
2018-09-11 10:28:38 +0800

    以minw bash方式打开git

--- diyQuickKey.py
+++ diyQuickKey.py
@@ -35,7 +35,7 @@ class OpenGitCommand(sublime_plugin.TextCommand):
                 if(f == '.git'):
                     # 打开git
                     chdir = 'cd "%s"' % path
-                    os.system(chdir+' && start "%ProgramFiles%\\Git\\git-bash.exe" && exit')
+                    os.system(chdir+' && start "" "%ProgramFiles%\\Git\\git-bash.exe" && exit')
                     return
 
             # 切换到上一级
